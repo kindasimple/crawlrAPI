870ed3b7a2e349647071aabf235c290a073747f8
Evan White <kindasimple@gmail.com>
2014-04-06 07:55:06 -0400

    Support put request for routes from the command line

--- worker.js
+++ worker.js
@@ -12,6 +12,7 @@ var path = require('path');
 var sys = require('sys')
 var exec = require('child_process').exec;
 var fs = require('fs');
+var util = require('util')
 var app = express();
 
 // all environments
@@ -54,20 +55,24 @@ function guid() {
 app.get('/result/:guid', function(req, res){
 
 	var id = req.param("guid");
+	var filePath = './public/' + id + '.log';
+	fs.exists(filePath, function(exists) {
 
-	fs.readFile('./public/' + id + '.log', 'utf8', function (err,data) {
-		if (err) {
-			return console.log(err);
-		}
+	  if (exists) {
+		fs.readFile(filePath, 'utf8', function (err,data) {
+			if (err) {
+			  res.status(500).send('Server Error. Unable to read result.')
+			  return console.log(err);
+		    }
 
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.write(data);
-
-		
-		res.end();
-	});
-
-
+		      res.writeHead(200, {'Content-Type': 'application/json'});
+		      res.write(data);
+		      res.end();
+	      });
+	  } else { //file doesn't exist
+	    res.status(404).send('Result not found. Your request is still processing.');
+	  }
+	}); //fs.exists
 });
 
 // //http://crawlr.ngrok.com/route/bar%20blue
@@ -84,6 +89,27 @@ app.get('/route/:bar', function(req, res){
     res.end();
 });
 
+app.post('/route/:bar', function(req, res){
+	var survey = {};
+	survey.cost = parseInt(req.body.cost);
+	survey.alcohol = parseInt(req.body.alcohol);
+	survey.distance = parseInt(req.body.distance);
+
+	console.log(survey);
+
+	var bar = req.param("bar");
+	var uuid = guid();
+
+	var command = util.format("rscript branchandboundproto.R \"{0}\" \"{1}\" \"{2}\" \"{3}\" \"{4}\"", bar, survey.cost, survey.alcohol, survey.distance, uuid)
+
+
+	function puts(error, stdout, stderr) { sys.puts(stdout) }
+	exec(command, puts);
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(uuid)
+    res.end();
+});
 
 
 http.createServer(app).listen(app.get('port'), function(){
