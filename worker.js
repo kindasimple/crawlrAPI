b5702f1a09ca4ba52a76c80c165cb5cda6f808ca
Evan White <kindasimple@gmail.com>
2014-04-06 12:33:44 -0400

    Add json feed

--- worker.js
+++ worker.js
@@ -24,6 +24,11 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+    });
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,9 +41,21 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 var toJson = function (data) {
-	return { 
-		bars: data.split('\n') 
-	};
+	var result = { tours: []};
+  	//parse output and create json
+  	data.split("\n\n").forEach(function (line) {
+  		var tour = { bars: [] };
+  			tour.title = "";
+			line.split('\n').forEach(function (bar) {
+				tour.bars.push(bar);
+			});
+			result.tours.push(tour);
+  	})
+
+  	result.tours[0].title = 'The Best Tour for you';
+  	result.tours[1].title = 'The Best Tour with PageRank Considerations';
+  	result.tours[2].title = 'The second Best Tour with PageRank Considerations';
+	return JSON.stringify(result);
 }
 
 function guid() {
@@ -50,30 +67,6 @@ function guid() {
   return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
          s4() + '-' + s4() + s4() + s4();
 }
-
-//http://crawlr.ngrok.com/route/bar%20blue/result
-app.get('/result/:guid', function(req, res){
-
-	var id = req.param("guid");
-	var filePath = './public/' + id + '.log';
-	fs.exists(filePath, function(exists) {
-
-	  if (exists) {
-		fs.readFile(filePath, 'utf8', function (err,data) {
-			if (err) {
-			  res.status(500).send('Server Error. Unable to read result.')
-			  return console.log(err);
-		    }
-
-		      res.writeHead(200, {'Content-Type': 'application/json'});
-		      res.write(data);
-		      res.end();
-	      });
-	  } else { //file doesn't exist
-	    res.status(404).send('Result not found. Your request is still processing.');
-	  }
-	}); //fs.exists
-});
 
 // //http://crawlr.ngrok.com/route/bar%20blue
 app.get('/route/:bar', function(req, res){
@@ -110,6 +103,63 @@ app.post('/route/:bar', function(req, res){
     res.write(uuid)
     res.end();
 });
+
+//http://crawlr.ngrok.com/route/bar%20blue/result
+app.get('/result/:guid', function(req, res){
+
+	var id = req.param("guid");
+	var extension = req.param("extension");
+	var filePath = './public/' + id + '.log';
+	fs.exists(filePath, function(exists) {
+
+	  if (exists) {
+		fs.readFile(filePath, 'utf8', function (err,data) {
+			if (err) {
+			  res.status(500).send('Server Error. Unable to read result.')
+			  return console.log(err);
+		    }
+
+		      res.writeHead(200, {'Content-Type': 'application/json'});
+		      res.write(data);
+		      
+		      res.end();
+	      });
+	  } else { //file doesn't exist
+	    res.status(404).send('Result not found. Your request is still processing.');
+	  }
+	}); //fs.exists
+});
+
+app.get('/result/:guid/:extension', function(req, res){
+
+	var id = req.param("guid");
+	var extension = req.param("extension");
+	var filePath = './public/' + id + '.log';
+	fs.exists(filePath, function(exists) {
+
+	  if (exists) {
+		fs.readFile(filePath, 'utf8', function (err,data) {
+			if (err) {
+			  res.status(500).send('Server Error. Unable to read result.')
+			  return console.log(err);
+		    }
+
+		      res.writeHead(200, {'Content-Type': 'application/json'});
+		      if(extension == null) {
+		      	res.write(data);
+		      } else {
+		      	res.write(toJson(data));
+		      }
+		      
+		      res.end();
+	      });
+	  } else { //file doesn't exist
+	    res.status(404).send('Result not found. Your request is still processing.');
+	  }
+	}); //fs.exists
+});
+
+
 
 
 http.createServer(app).listen(app.get('port'), function(){
